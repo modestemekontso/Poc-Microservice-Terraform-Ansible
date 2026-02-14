b86aefb01e62fb93d26f3098fb2d4b3c0e8e5406
koffimensah <mensahson74@gmail.com>
2026-02-14 06:05:00 -0500

    updating ci-cd pipeline

--- Poc_infra/terraform/modules/nain.tf
+++ Poc_infra/terraform/modules/nain.tf
@@ -1,20 +1,10 @@
-terraform {
-  required_version = ">= 1.5.0"
-  
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  
   backend "s3" {
     bucket         = "your-terraform-state-bucket"
     key            = "microservices/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
-}
+
 
 provider "aws" {
   region = var.aws_region
